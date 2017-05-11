3bc90b558c86942e5e4ac5ce9f2f0e31ee28d88e
Josie Lipkin <jlipkin8@gmail.com>
2017-05-11 16:56:13 -0700

    defined view_random_compliment and imported the random library so I could use the randint function

--- compliments.py
+++ compliments.py
@@ -1,6 +1,8 @@
 """ This is a Terminal-based program that allows a user to create, view the entire, or view a 
 random compliment from the compliment list """
 
+import ranint from random 
+
 def add_compliment_list(my_list):
     """takes user input and adds it as a new item to the end of the list"""
     compliment = raw_input("Give a compliment: ")
@@ -12,3 +14,8 @@ def view_all_compliments(my_list):
     for compliment in my_list: 
         print compliment 
 
+def view_random_compliment(my_list): 
+    end = my_list - 1 
+    random = randint(0,end)
+    print my_list[random]
+    
