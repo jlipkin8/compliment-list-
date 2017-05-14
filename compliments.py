d8da917ffa1edd88ec555e0375262fe869e2d2d0
Josie Lipkin <jlipkin8@gmail.com>
2017-05-14 13:32:07 -0700

    added a display menu that allows the user to choose what to do

--- compliments.py
+++ compliments.py
@@ -1,7 +1,9 @@
 """ This is a Terminal-based program that allows a user to create, view the entire, or view a 
 random compliment from the compliment list """
 
-import ranint from random 
+""" Borrowed so much from the todo_list workshop code """ 
+
+import random 
 
 def add_compliment_list(my_list):
     """takes user input and adds it as a new item to the end of the list"""
@@ -16,6 +18,26 @@ def view_all_compliments(my_list):
 
 def view_random_compliment(my_list): 
     end = my_list - 1 
-    random = randint(0,end)
-    print my_list[random]
-    
+    random_num = random.randint(0,end)
+    print my_list[random_num]
+
+def display_main_menu(my_list): 
+    """Displays main option, takes in user input, calls view all, view random, or
+    add function """ 
+
+    user_options = """ 
+        \nWould you like to: 
+        A. Add a compliment
+        B. View all the compliments 
+        C. View a random compliment 
+        D. Quit the program 
+        >>> """ 
+
+        while True: 
+            user_input = raw_input(user_options) 
+            if user_input == "A": 
+            elif user_input == "B": 
+            elif user_input == "C": 
+            elif user_input == "D": 
+            else: 
+                print "Thats not a valid option, please enter A, B, C, or D"
